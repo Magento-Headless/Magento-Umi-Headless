c849d0abaa74adda546369f9e945e1899c791d3b
caesar <caesar.luo@silksoftware.com>
2022-05-11 16:52:23 +0800

    chore: pwa framework

--- .umirc.js
+++ .umirc.js
@@ -1,6 +1,11 @@
 
 // ref: https://umijs.org/config/
 export default {
+  base: '/',
+  target: {
+    ie: 11
+  },
+  hase: true,
   treeShaking: true,
   routes: [
     {
@@ -11,27 +16,40 @@ export default {
       ]
     }
   ],
+  alias: {
+    '@': resolve(__dirname, '../src')
+  },
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true,
-      dynamicImport: { webpackChunkName: true },
-      title: 'magento-umi-headless',
-      dll: true,
-      locale: {
-        enable: true,
-        default: 'en-US',
-      },
-      routes: {
-        exclude: [
-          /models\//,
-          /services\//,
-          /model\.(t|j)sx?$/,
-          /service\.(t|j)sx?$/,
-          /components\//,
-        ],
-      },
-    }],
-  ],
+    [
+      'umi-plugin-react', {
+        antd: true,
+        dva: {
+          dynamicImport: true,
+          hmr: true
+        },
+        dynamicImport: {
+          webpackChunkName: true
+        },
+        title: 'magento-umi-headless',
+        dll: {
+          exclude: [],
+          include: []
+        },
+        locale: {
+          enable: true,
+          default: 'en-US'
+        },
+        routes: {
+          exclude: [
+            /models\//,
+            /services\//,
+            /model\.(t|j)sx?$/,
+            /service\.(t|j)sx?$/,
+            /components\//
+          ]
+        }
+      }
+    ]
+  ]
 }
