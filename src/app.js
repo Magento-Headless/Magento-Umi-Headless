b4359eeed2b0283001424cef55307502f10e91b3
caesar <454451758@qq.com>
2022-05-12 16:06:54 +0800

    chore: yarn workspace

--- src/app.js
+++ src/app.js
@@ -1,21 +1,25 @@
-import { ConfigProvider, message } from 'antd';
-import { createLogger } from 'redux-logger';
+import { ConfigProvider, message } from 'antd'
+import { ThemeProvider } from 'styled-components'
+import { createLogger } from 'redux-logger'
 
-import { ThemeConf } from '../config/theme.confg';
+import AppShell from '@components/AppShell'
+import { ThemeConf } from '../config/theme.confg'
 
 export const rootContainer = (container) => {
   return (
-    <ConfigProvider prefixCls={ThemeConf.prefix}>
-      <div>{container}</div>
-    </ConfigProvider>
-  );
-};
+    <ThemeProvider theme={ThemeConf.theme}>
+      <ConfigProvider prefixCls={ThemeConf.prefix}>
+        <AppShell>{container}</AppShell>
+      </ConfigProvider>
+    </ThemeProvider>
+  )
+}
 
 export const dva = {
   config: {
     onAction: createLogger(),
     onError: (err) => {
-      message.error(err.message, 3);
-    },
-  },
-};
+      message.error(err.message, 3)
+    }
+  }
+}
